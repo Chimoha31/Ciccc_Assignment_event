218363fd069ffcd88fd5977b80c6a97cbe1f406b
Chimoha31 <mokochii1108@gmail.com>
2022-01-25 23:55:05 -0800

    Add icons

--- NoteManager/solution.js
+++ NoteManager/solution.js
@@ -1,26 +1,27 @@
 const input = document.querySelector("#add-input");
 const addButton = document.querySelector("#add-btn");
-const ul = document.querySelector('#list');
-const li = document.createElement('li');
-
-
+const ul = document.querySelector("#list");
+const li = document.createElement("li");
 
 addButton.addEventListener("click", function (e) {
   e.preventDefault();
   const inputText = input.value;
   console.log(inputText);
-  
-  if(!input.value) {
+
+  if (!input.value) {
     return;
   } else {
-    li.innerText = inputText;
+    li.innerHTML = `<p>${inputText}</p>
+        <p>
+        <i class="fa fa-pencil"></i>
+        <i class="fa fa-times"></i>
+      </p>`;
     console.log(li);
     ul.appendChild(li);
   }
 });
 
-
 // Addボタンを押したら内容をクリアにする
-addButton.addEventListener("click", function() {
-  input.value = ''
+addButton.addEventListener("click", function () {
+  input.value = "";
 });
