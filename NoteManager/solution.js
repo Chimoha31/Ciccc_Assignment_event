65245524d32de85718227e2f0133e06640292e99
Chimoha31 <mokochii1108@gmail.com>
2022-01-26 00:17:54 -0800

    Another Solution DONE

--- NoteManager/solution.js
+++ NoteManager/solution.js
@@ -6,19 +6,27 @@ const li = document.createElement("li");
 addButton.addEventListener("click", function (e) {
   e.preventDefault();
   const inputText = input.value;
-  console.log(inputText);
 
-  if (!input.value) {
-    return;
-  } else {
-    li.innerHTML = `<p>${inputText}</p>
-        <p>
-        <i class="fa fa-pencil"></i>
-        <i class="fa fa-times"></i>
-      </p>`;
-    console.log(li);
-    ul.appendChild(li);
-  }
+// Solution2 (複数生成あり)
+ const copy = ul.firstElementChild.cloneNode(true);
+ copy.firstElementChild.innerText = inputText;
+ ul.appendChild(copy);
+ console.log(copy);
+ 
+
+
+// Solution1 (複数生成なし)
+  // if (!input.value) {
+  //   return;
+  // } else {
+  //   li.innerHTML = `<p>${inputText}</p>
+  //       <p>
+  //       <i class="fa fa-pencil"></i>
+  //       <i class="fa fa-times"></i>
+  //     </p>`;
+  //   // console.log(li);
+  //   ul.appendChild(li);
+  // }
 });
 
 // Addボタンを押したら内容をクリアにする
